213758636ba2115f6a17a0acd8cd124136e0ef3e
vinicius-daros <viniciush.dev@gmail.com>
2026-04-16 17:28:37 -0300

    Refactor display connection and invert HEX0 output
    Updated display connection to use hex_temp wire and inverted HEX0 output.

--- labs/05_halfadder/top.v
+++ labs/05_halfadder/top.v
@@ -3,8 +3,9 @@ module top(
   output [6:0] HEX0); // a, b, c, d, e, f, g
   // instancie e conecte os módulos a seguir
   wire fio0, fio1;
+  wire [6:0] hex_temp;
   adder adder0(SW[0], SW[1], fio0, fio1);
-  display display0(fio0, fio1, HEX0[0], HEX0[1], HEX0[2], HEX0[3], HEX0[4], HEX0[5], HEX0[6]);
-
+  display display0(fio0, fio1, hex_temp[0], hex_temp[1], hex_temp[2], hex_temp[3], hex_temp[4], hex_temp[5], hex_temp[6]);
+  assign HEX0 = ~hex_temp;
 
 endmodule
